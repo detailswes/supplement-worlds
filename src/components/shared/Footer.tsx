c260dab3e1091ec546f8a029c98a0840ed45d5a9
details.wes <details.wes@gmail.com>
2025-03-09 19:45:43 +0530

    mobile issue testing

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full px-4 sm:px-16 pb-2 sm:pb-0">
+    <footer className="w-full px-4 sm:px-16 pb-0 sm:pb-0">
       <p className="text-right text-xs  sm:text-sm text-[#C6C7F8] sm:text-white font-medium">
         AGB | Datenschutz | Impressum
       </p>
